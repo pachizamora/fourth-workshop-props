d38c6839044757ff8afd35f563c8d4562ff05bce
pachizamora <maripaz.zamora@gmail.com>
2022-02-20 13:18:20 -0600

    4th React Workshop - Props

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,28 @@ import Table from "./Table";
 
 class App extends React.Component {
   render() {
+    const characters = [
+      {
+        name: "John",
+        job: "Smithery",
+      },
+      {
+        name: "Clyde",
+        job: "Carpenter",
+      },
+      {
+        name: "Wanda",
+        job: "Engineer",
+      },
+      {
+        name: "Claudia",
+        job: "Manager",
+      },
+    ];
+
     return (
       <div className="container">
-        <Table />
+        <Table characters={characters} />
       </div>
     );
   }
